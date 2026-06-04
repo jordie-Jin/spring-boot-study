fb45e0da9662bcf1d91d83d1951174828b86618b
jojo <jordie-Jin@github.com>
2026-06-04 16:02:05 +0900

    고Clothes 고도화

--- src/main/java/com/sesac/ai/backend/domain/Clothes.java
+++ src/main/java/com/sesac/ai/backend/domain/Clothes.java
@@ -11,6 +11,19 @@ import org.springframework.stereotype.Service;
 public class Clothes {
 
     private Long id;
-    private String name;
-    private int price;
+
+    private String name;        // 상품명
+    private String category;    // 상의, 하의, 아우터
+
+    private String brand;       // 브랜드
+    private String color;       // 색상
+    private String size;        // M, L, XL
+
+    private Integer price;      // 가격
+    private Integer stock;      // 재고
+
+    private String material;    // 소재
+    private String season;      // 봄, 여름, 가을, 겨울
+
+    private Boolean soldOut;    // 품절 여부
 }
